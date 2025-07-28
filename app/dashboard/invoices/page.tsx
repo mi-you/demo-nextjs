ad6bbec7224d11e99e881c0137c35ef48b6d1772
Lou <loushuangzhanbj@kanyun.com>
2025-07-28 17:43:20 +0800

    done - chapter 10

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+import { fetchInvoicesPages } from '@/app/lib/data';
+
+export default async function Page() {
+  // 用于验证 route group（overview） 前后访问 invoices 页面时，loading 组件的显示情况
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return <p>Invoices Page</p>;
 }
